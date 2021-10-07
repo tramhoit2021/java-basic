29b8e5875807e411761bf667ec289227f8973d27
TRAM <maitram1982vn@gmail.com>
2021-10-07 23:52:35 +0700

    1.test 1

---
+++ baitap/src/test/java/TestXuatChuoi.java
@@ -0,0 +1,6 @@
+public class TestXuatChuoi {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("My name Trâm");
+    }
+}
---
+++ baitap/src/test/java/TongHaiSo.java
@@ -0,0 +1,7 @@
+public class TongHaiSo {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 36;
+        System.out.println("Tổng của x+y ="  +(x+y));
+    }
+}
